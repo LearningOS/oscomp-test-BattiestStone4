4027ea6ad29b19012a2b512c4f8489e88c0b280c
Yichen Lin <2848327718@qq.com>
2025-04-14 18:24:26 +0800

    fix again

--- .arceos/api/arceos_posix_api/src/imp/fs.rs
+++ .arceos/api/arceos_posix_api/src/imp/fs.rs
@@ -29,7 +29,7 @@ impl File {
         super::fd_ops::add_file_like(Arc::new(self))
     }
 
-    pub fn from_fd(fd: c_int) -> LinuxResult<Arc<Self>> {
+    fn from_fd(fd: c_int) -> LinuxResult<Arc<Self>> {
         let f = super::fd_ops::get_file_like(fd)?;
         f.into_any()
             .downcast::<Self>()
--- src/syscall.rs
+++ src/syscall.rs
@@ -1,21 +1,46 @@
-use axerrno::{LinuxError, LinuxResult};
+use axerrno::LinuxError;
 use axhal::{
     arch::TrapFrame,
     trap::{SYSCALL, register_trap_handler},
 };
+use linux_raw_sys::general::SIGSYS;
 use starry_api::*;
 use starry_core::task::{time_stat_from_kernel_to_user, time_stat_from_user_to_kernel};
 use syscalls::Sysno;
 
 #[register_trap_handler(SYSCALL)]
-fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
-    info!("[syscall] <{:?}> begin", Sysno::from(syscall_num as u32));
+fn handle_syscall(tf: &mut TrapFrame, syscall_num: usize) -> isize {
+    let sysno = Sysno::from(syscall_num as u32);
+    info!("Syscall {}", sysno);
     time_stat_from_user_to_kernel();
-    let result: LinuxResult<isize> = match Sysno::from(syscall_num as u32) {
+
+    let result = match sysno {
         Sysno::read => sys_read(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
+        Sysno::readv => sys_readv(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
         Sysno::write => sys_write(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
-        Sysno::mmap => sys_mmap(
+        Sysno::writev => sys_writev(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
+        Sysno::pread64 => sys_pread64(
+            tf.arg0() as _,
+            tf.arg1().into(),
+            tf.arg2() as _,
+            tf.arg3() as _,
+        ),
+        Sysno::sendfile => sys_sendfile(
+            tf.arg0() as _,
+            tf.arg1() as _,
+            tf.arg2().into(),
+            tf.arg3() as _,
+        ),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::poll => sys_poll(tf.arg0().into(), tf.arg1() as _, tf.arg2() as _),
+        Sysno::ppoll => sys_ppoll(
             tf.arg0().into(),
+            tf.arg1() as _,
+            tf.arg2().into(),
+            tf.arg3().into(),
+        ),
+        Sysno::mmap => sys_mmap(
+            tf.arg0(),
             tf.arg1() as _,
             tf.arg2() as _,
             tf.arg3() as _,
@@ -23,17 +48,21 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
             tf.arg5() as _,
         ),
         Sysno::ioctl => sys_ioctl(tf.arg0() as _, tf.arg1() as _, tf.arg2().into()),
-        Sysno::writev => sys_writev(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
         Sysno::sched_yield => sys_sched_yield(),
         Sysno::nanosleep => sys_nanosleep(tf.arg0().into(), tf.arg1().into()),
         Sysno::getpid => sys_getpid(),
         Sysno::getppid => sys_getppid(),
+        Sysno::gettid => sys_gettid(),
         Sysno::exit => sys_exit(tf.arg0() as _),
+        Sysno::exit_group => sys_exit_group(tf.arg0() as _),
         Sysno::gettimeofday => sys_get_time_of_day(tf.arg0().into()),
         Sysno::getcwd => sys_getcwd(tf.arg0().into(), tf.arg1() as _),
         Sysno::dup => sys_dup(tf.arg0() as _),
-        Sysno::dup3 => sys_dup3(tf.arg0() as _, tf.arg1() as _),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::dup2 => sys_dup2(tf.arg0() as _, tf.arg1() as _),
+        Sysno::dup3 => sys_dup2(tf.arg0() as _, tf.arg1() as _),
         Sysno::fcntl => sys_fcntl(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
+        Sysno::lseek => sys_lseek(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
         Sysno::clone => sys_clone(
             tf.arg0() as _,
             tf.arg1() as _,
@@ -41,8 +70,12 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
             tf.arg3() as _,
             tf.arg4() as _,
         ),
-        Sysno::wait4 => sys_wait4(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
-        Sysno::pipe2 => sys_pipe2(tf.arg0().into()),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::fork => sys_fork(),
+        Sysno::wait4 => sys_waitpid(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
+        Sysno::pipe2 => sys_pipe(tf.arg0().into()),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::pipe => sys_pipe(tf.arg0().into()),
         Sysno::close => sys_close(tf.arg0() as _),
         Sysno::chdir => sys_chdir(tf.arg0().into()),
         Sysno::mkdirat => sys_mkdirat(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
@@ -63,8 +96,14 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
             tf.arg3().into(),
             tf.arg4() as _,
         ),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::link => sys_link(tf.arg0().into(), tf.arg1().into()),
         Sysno::unlinkat => sys_unlinkat(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::unlink => sys_unlink(tf.arg0().into()),
         Sysno::uname => sys_uname(tf.arg0().into()),
+        #[cfg(target_arch = "x86_64")]
+        Sysno::stat => sys_stat(tf.arg0().into(), tf.arg1().into()),
         Sysno::fstat => sys_fstat(tf.arg0() as _, tf.arg1().into()),
         Sysno::mount => sys_mount(
             tf.arg0().into(),
@@ -95,16 +134,25 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
             tf.arg3() as _,
             tf.arg4().into(),
         ),
-        Sysno::munmap => sys_munmap(tf.arg0().into(), tf.arg1() as _),
-        Sysno::mprotect => sys_mprotect(tf.arg0().into(), tf.arg1() as _, tf.arg2() as _),
+        Sysno::statfs => sys_statfs(tf.arg0().into(), tf.arg1().into()),
+        Sysno::munmap => sys_munmap(tf.arg0(), tf.arg1() as _),
+        Sysno::mprotect => sys_mprotect(tf.arg0(), tf.arg1() as _, tf.arg2() as _),
         Sysno::times => sys_times(tf.arg0().into()),
         Sysno::brk => sys_brk(tf.arg0() as _),
+        Sysno::prlimit64 => sys_prlimit64(
+            tf.arg0() as _,
+            tf.arg1() as _,
+            tf.arg2().into(),
+            tf.arg3().into(),
+        ),
         #[cfg(target_arch = "x86_64")]
         Sysno::arch_prctl => sys_arch_prctl(tf.arg0() as _, tf.arg1().into()),
         Sysno::set_tid_address => sys_set_tid_address(tf.arg0().into()),
         Sysno::clock_gettime => sys_clock_gettime(tf.arg0() as _, tf.arg1().into()),
-        Sysno::exit_group => sys_exit_group(tf.arg0() as _),
         Sysno::getuid => sys_getuid(),
+        Sysno::geteuid => sys_geteuid(),
+        Sysno::getgid => sys_getgid(),
+        Sysno::getegid => sys_getegid(),
         Sysno::rt_sigprocmask => sys_rt_sigprocmask(
             tf.arg0() as _,
             tf.arg1().into(),
@@ -117,36 +165,32 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
             tf.arg2().into(),
             tf.arg3() as _,
         ),
-        #[cfg(target_arch = "x86_64")]
-        Sysno::fork => sys_fork(),
-        Sysno::gettid => sys_gettid(),
-        Sysno::lseek => sys_lseek(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
-        Sysno::prlimit64 => sys_prlimit64(
-            tf.arg0() as _,
-            tf.arg1() as _,
-            tf.arg2().into(),
-            tf.arg3().into(),
-        ),
-        Sysno::readv => sys_readv(tf.arg0() as _, tf.arg1().into(), tf.arg2() as _),
         Sysno::rt_sigtimedwait => sys_rt_sigtimedwait(
-            tf.arg0() as _,
+            tf.arg0().into(),
             tf.arg1().into(),
             tf.arg2().into(),
             tf.arg3() as _,
         ),
-        Sysno::statfs => sys_statfs(tf.arg0().into(), tf.arg1().into()),
-
-        _ => {
-            warn!("Unimplemented syscall: {}", syscall_num);
-            axtask::exit(LinuxError::ENOSYS as _)
+        Sysno::rt_sigsuspend => sys_rt_sigsuspend(tf, tf.arg0().into(), tf.arg1() as _),
+        Sysno::rt_sigpending => sys_rt_sigpending(tf.arg0().into(), tf.arg1() as _),
+        Sysno::rt_sigreturn => sys_rt_sigreturn(tf),
+        Sysno::kill => sys_kill(tf.arg0() as _, tf.arg1() as _),
+        Sysno::tgkill => sys_tgkill(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
+        Sysno::futex => {
+            warn!("preventing pthread from blocking testing");
+            do_exit(SIGSYS as _, true);
+        }
+        sysno => {
+            warn!("Unimplemented syscall: {}", sysno);
+            Err(LinuxError::ENOSYS)
         }
     };
-    let ans = result.unwrap_or_else(|err| -err.code() as _);
+    let result = result.unwrap_or_else(|err| -err.code() as isize);
     time_stat_from_kernel_to_user();
     info!(
-        "[syscall] <{:?}> return {}",
+        "Syscall {:?} return {:?}",
         Sysno::from(syscall_num as u32),
-        ans
+        result,
     );
-    ans
+    result
 }
